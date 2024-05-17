crucial bug fix in dirichlet score computation; some other small updates

src/AD_modules.py
@@ -39,7 +39,7 @@ def gaussian_scoring_2_moments(obs,
 
 def dirichlet_scoring(
         obs, cond_exp, cond_var, observed_dates,
-        nb_samples=10**5,
+        epsilon=1e-6, nb_samples=10**5,
         min_var_val=1e-5, replace_var=None, verbose=False,
         seed=1):
     # obs : [nb_steps, nb_samples, dimension]
@@ -69,7 +69,7 @@ def dirichlet_scoring(
             if observed_dates[t,s]:
                 if seed is not None:
                     np.random.seed(seed)
-                E = np.maximum(cond_exp[t,s], 1e-10)  # only positive values
+                E = np.maximum(cond_exp[t,s], epsilon)  # only positive values
                 E = E/np.sum(E)  # normalize s.t. sum = 1
                 # TODO: here we could try to find the ind with the best variance prediction
                 factors = (E*(1-E))/cond_var[t,s] - 1
@@ -86,23 +86,23 @@ def dirichlet_scoring(
                 diri = stat.dirichlet(alpha)
                 rvs = diri.rvs(size=nb_samples)
                 pdfs = np.array(
-                    [diri.logpdf(check_dirichlet_input(r)) for r in rvs])
-                obs_pdf = diri.logpdf(check_dirichlet_input(obs[t,s]))
-                pval = np.mean(obs_pdf <= pdfs)
-                scores[t,s] = -np.log(pval + 1e-10)
+                    [diri.logpdf(check_dirichlet_input(r, epsilon)) for r in rvs])
+                obs_pdf = diri.logpdf(check_dirichlet_input(obs[t,s], epsilon))
+                pval = np.mean(obs_pdf > pdfs)
+                scores[t,s] = -np.log(pval + epsilon)
 
     # scores : [nb_steps, nb_samples]
     return scores
 
 
-def check_dirichlet_input(sample):
+def check_dirichlet_input(sample, epsilon=1e-14):
     """
     check if the input sample is a valid dirichlet distribution
     :param sample: np.array, the sample
     """
     if np.any(sample <= 0):
         # print(np.min(sample))
-        sample = np.maximum(sample, 1e-14)
+        sample = np.maximum(sample, epsilon)
     if np.sum(sample) != 1:
         # to avoid rounding errors, which can break the code
         sample = sample.astype(np.double)
@@ -576,6 +576,7 @@ class Simple_AD_module(torch.nn.Module):  # AD_module_1D, AD_module_ND
                  replace_values = None,
                  class_thres = 0.5,
                  nb_MC_samples = 10**5,
+                 epsilon=1e-6,
                  seed=None,
                  verbose=False,):
         super(Simple_AD_module, self).__init__()
@@ -587,6 +588,7 @@ class Simple_AD_module(torch.nn.Module):  # AD_module_1D, AD_module_ND
         self.nb_samples = nb_MC_samples
         self.verbose = verbose
         self.seed = seed
+        self.epsilon = epsilon
 
         self.weight = score_factor
 
@@ -595,7 +597,8 @@ class Simple_AD_module(torch.nn.Module):  # AD_module_1D, AD_module_ND
         elif activation_fct == 'id':
             self.act = torch.nn.Identity()
 
-    def get_individual_scores(self, cond_moments, obs, observed_dates=None):
+    def get_individual_scores(
+            self, cond_moments, obs, observed_dates=None):
 
         assert(('id' in self.output_vars) and (
                 ('var' in self.output_vars) or ('power-2' in self.output_vars)))
@@ -620,7 +623,7 @@ class Simple_AD_module(torch.nn.Module):  # AD_module_1D, AD_module_ND
             # scores : [nb_steps, nb_samples]
             scores_valid = dirichlet_scoring(
                 obs=obs, cond_exp=cond_exp, cond_var=cond_var,
-                observed_dates=observed_dates,
+                observed_dates=observed_dates, epsilon=self.epsilon,
                 nb_samples=self.nb_samples, replace_var=None, min_var_val=0.,
                 verbose=self.verbose, seed=self.seed)
             scores_valid = scores_valid.transpose(1,0)
@@ -632,7 +635,7 @@ class Simple_AD_module(torch.nn.Module):  # AD_module_1D, AD_module_ND
 
         scores = self.get_individual_scores(cond_moments, obs, observed_dates)
 
-        # scores : [nb_steps, nb_samples, dimension] or [nb_steps, nb_samples]
+        # scores : [nb_steps, nb_samples, dimension] or [nb_samples, nb_steps]
         return scores
 
     def get_predicted_label(self, obs, cond_moments, observed_dates=None):

src/Microbial_AD_eval.py
@@ -29,6 +29,11 @@ import data_utils
 from AD_modules import AD_module, Simple_AD_module
 import AD_modules
 
+try:
+    from telegram_notifications import send_bot_message as SBM
+except Exception:
+    from configs.config import SendBotMessage as SBM
+
 
 # =====================================================================================================================
 # FLAGS
@@ -175,7 +180,9 @@ def compute_scores(
         n_dataset_workers=None,
         nb_MC_samples=10**5,
         verbose=False,
+        epsilon=1e-6,
         seed=333,
+        send=False,
         **options
 ):
     """
@@ -194,6 +201,7 @@ def compute_scores(
         nb_MC_samples: int, number of MC samples for approximating the pvalue
             when using the dirichlet distribution
         verbose: bool, whether to print the progress
+        epsilon: float, small value to avoid division by zero etc.
         seed: int, seed for reproducibility
     """
     global USE_GPU, N_CPUS, N_DATASET_WORKERS
@@ -291,6 +299,7 @@ def compute_scores(
         replace_values=None,
         class_thres=class_thres,
         seed=seed,
+        epsilon=epsilon,
         verbose=verbose)
 
     # train data
@@ -301,7 +310,11 @@ def compute_scores(
     with open('{}train_ad_scores.npy'.format(scores_path), 'wb') as f:
         np.save(f, ad_scores)
         np.save(f, abx_labels)
-    del cond_moments, observed_dates, true_X, abx_labels, ad_scores
+    data = np.concatenate([abx_labels.reshape(-1, 1), ad_scores], axis=1)
+    cols = ['abx'] + ['ad_score_{}'.format(i) for i in range(ad_scores.shape[1])]
+    df = pd.DataFrame(data, columns=cols)
+    csvpath = '{}train_ad_scores.csv'.format(scores_path)
+    df.to_csv(csvpath, index=False)
 
     # test data
     cond_moments, observed_dates, true_X, abx_labels = get_model_predictions(
@@ -311,12 +324,27 @@ def compute_scores(
     with open('{}val_ad_scores.npy'.format(scores_path), 'wb') as f:
         np.save(f, ad_scores)
         np.save(f, abx_labels)
-    del cond_moments, observed_dates, true_X, abx_labels, ad_scores
+    data = np.concatenate([abx_labels.reshape(-1, 1), ad_scores], axis=1)
+    cols = ['abx'] + ['ad_score_{}'.format(i) for i in
+                      range(ad_scores.shape[1])]
+    df = pd.DataFrame(data, columns=cols)
+    csvpath_val = '{}val_ad_scores.csv'.format(scores_path)
+    df.to_csv(csvpath_val, index=False)
+
+    if send:
+        files_to_send = [csvpath, csvpath_val]
+        caption = "scores - {} - id={}".format(which, forecast_model_id)
+        SBM.send_notification(
+            text=None,
+            chat_id=config.CHAT_ID,
+            files=files_to_send,
+            text_for_files=caption
+        )
 
 
 def evaluate_scores(
         forecast_saved_models_path, forecast_model_id=None, load_best=True,
-        validation=False, **options):
+        validation=False, send=False, **options):
     """
     Evaluate the anomaly detection scores
 
@@ -343,39 +371,46 @@ def evaluate_scores(
         val_ad_scores = np.load(f)
         val_abx_labels = np.load(f)
 
-    if not validation:
-        abx_samples = train_ad_scores[train_abx_labels == 1]
-        non_abx_samples = train_ad_scores[train_abx_labels == 0]
-        postfix = 'train'
-    else:
-        abx_samples = val_ad_scores[val_abx_labels == 1]
-        non_abx_samples = val_ad_scores[val_abx_labels == 0]
-        postfix = 'val'
 
-    fig, ax = plt.subplots(4, 2, figsize=(6*2, 4*4))
-    ax[0, 0].hist(np.nanmin(abx_samples, axis=1), label='abx min', bins=50)
-    ax[0, 1].hist(np.nanmin(non_abx_samples, axis=1), label='non-abx min', bins=50)
-    ax[1, 0].hist(np.nanmax(abx_samples, axis=1), label='abx max', bins=50)
-    ax[1, 1].hist(np.nanmax(non_abx_samples, axis=1), label='non-abx max', bins=50)
-    ax[2, 0].hist(np.nanmean(abx_samples, axis=1), label='abx mean', bins=50)
-    ax[2, 1].hist(np.nanmean(non_abx_samples, axis=1), label='non-abx mean', bins=50)
-    ax[3, 0].hist(np.nanmedian(abx_samples, axis=1), label='abx median', bins=50)
-    ax[3, 1].hist(np.nanmedian(non_abx_samples, axis=1), label='non-abx median', bins=50)
-    ax[0, 0].set_title("abx")
-    ax[0, 1].set_title("non-abx")
-    ax[0, 0].set_ylabel("min")
-    ax[1, 0].set_ylabel("max")
-    ax[2, 0].set_ylabel("mean")
-    ax[3, 0].set_ylabel("median")
-    plt.tight_layout()
+    for postfix in ['train', 'val']:
+        if postfix == 'train':
+            abx_samples = train_ad_scores[train_abx_labels == 1]
+            non_abx_samples = train_ad_scores[train_abx_labels == 0]
+        else:
+            abx_samples = val_ad_scores[val_abx_labels == 1]
+            non_abx_samples = val_ad_scores[val_abx_labels == 0]
+            postfix = 'val'
+        fig, ax = plt.subplots(4, 2, figsize=(6*2, 4*4))
+        ax[0, 0].hist(np.nanmin(abx_samples, axis=1), label='abx min', bins=50)
+        ax[0, 1].hist(np.nanmin(non_abx_samples, axis=1), label='non-abx min', bins=50)
+        ax[1, 0].hist(np.nanmax(abx_samples, axis=1), label='abx max', bins=50)
+        ax[1, 1].hist(np.nanmax(non_abx_samples, axis=1), label='non-abx max', bins=50)
+        ax[2, 0].hist(np.nanmean(abx_samples, axis=1), label='abx mean', bins=50)
+        ax[2, 1].hist(np.nanmean(non_abx_samples, axis=1), label='non-abx mean', bins=50)
+        ax[3, 0].hist(np.nanmedian(abx_samples, axis=1), label='abx median', bins=50)
+        ax[3, 1].hist(np.nanmedian(non_abx_samples, axis=1), label='non-abx median', bins=50)
+        ax[0, 0].set_title("abx")
+        ax[0, 1].set_title("non-abx")
+        ax[0, 0].set_ylabel("min")
+        ax[1, 0].set_ylabel("max")
+        ax[2, 0].set_ylabel("mean")
+        ax[3, 0].set_ylabel("median")
+        plt.tight_layout()
 
-    plt.savefig(evaluation_path+'hist_'+postfix+'.pdf', format='pdf')
+        impath = evaluation_path+'hist_'+postfix+'.pdf'
+        plt.savefig(impath, format='pdf')
 
-    # print(np.all(np.isnan(abx_samples), axis=1).sum())
-    # print(np.all(np.isnan(non_abx_samples), axis=1).sum())
+        if send:
+            caption = "scores-histogram - {} - id={}".format(which, forecast_model_id)
+            SBM.send_notification(
+                text=None,
+                chat_id=config.CHAT_ID,
+                files=[impath],
+                text_for_files=caption
+            )
 
-
-
+        # print(np.all(np.isnan(abx_samples), axis=1).sum())
+        # print(np.all(np.isnan(non_abx_samples), axis=1).sum())
 
     # train_score = metrics.roc_auc_score(train_abx_labels, train_ad_scores)
     # val_score = metrics.roc_auc_score(val_abx_labels, val_ad_scores)
@@ -951,7 +986,6 @@ def main(arg):
                         from configs import config
                         data_dict = eval("config."+data_dict)
                     forecast_param["dataset"] = data_dict["model_name"]
-            # evaluate(anomaly_data_dict=anomaly_data_dict, **forecast_param)
             for ad_param in ad_params:
                 print("AD param: ", ad_param)
                 if FLAGS.evaluate:
@@ -964,6 +998,7 @@ def main(arg):
                         **ad_param)
                 if FLAGS.compute_scores:
                     compute_scores(
+                        send=FLAGS.SEND,
                         forecast_saved_models_path=forecast_saved_models_path,
                         forecast_model_id=forecast_model_ids[i],
                         forecast_param=forecast_param,
@@ -972,6 +1007,7 @@ def main(arg):
                         **ad_param)
                 if FLAGS.evaluate_scores:
                     evaluate_scores(
+                        send=FLAGS.SEND,
                         forecast_saved_models_path=forecast_saved_models_path,
                         forecast_model_id=forecast_model_ids[i],
                         **ad_param)
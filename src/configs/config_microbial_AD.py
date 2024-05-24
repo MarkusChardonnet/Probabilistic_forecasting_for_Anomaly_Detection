cbdcad076887d43f4ed857a9d19082f9409a8506
Florian Krach <florian.krach@me.com>
2024-05-24 12:24:30 +0200

    updates & fixes in config

--- src/configs/config_microbial_AD.py
+++ src/configs/config_microbial_AD.py
@@ -3,19 +3,61 @@ from configs.config_utils import get_parameter_array, get_dataset_overview, \
 
 seed = 398
 
+
+# ==============================================================================
+# FLORIAN
+# ==============================================================================
+
 # ------------------------------------------------------------------------------
 # AD for otu
 AD_microbial_otu3 = "{}saved_models_microbial_otu3/".format(data_path)
-AD_microbial_otu3_ids = [1000]
+AD_microbial_otu3_ids = [68]
 param_list_AD_microbial_otu = []
 param_dict_AD_microbial_otu = {
         "dataset": ['microbial_otu_sig_highab'],
         'saved_models_path': [AD_microbial_otu3],
-        'load_best': [True],
+        'load_best': [True,],
         'nb_MC_samples': [10**4],
+        'epsilon': [1e-8, 1e-6],
         'verbose': [True],
         'seed': [seed],
-        'validation': [False, True],
 }
 param_list_AD_microbial_otu += get_parameter_array(
         param_dict=param_dict_AD_microbial_otu)
+
+param_dict_AD_microbial_otu = {
+        "dataset": ['microbial_otu_sig_highab'],
+        'saved_models_path': [AD_microbial_otu3],
+        'load_best': [True,],
+        'nb_MC_samples': [10**4],
+        'epsilon': [1e-6],
+        'verbose': [True],
+        'seed': [seed],
+        'use_replace_values': [True],
+        'dirichlet_use_coord': [1, 4, 11, 65],
+}
+param_list_AD_microbial_otu1 = get_parameter_array(
+        param_dict=param_dict_AD_microbial_otu)
+
+
+# ------------------------------------------------------------------------------
+# AD for genus
+AD_microbial_genus3 = "{}saved_models_microbial_genus3/".format(data_path)
+AD_microbial_genus3_ids = []
+param_list_AD_microbial_genus = []
+param_dict_AD_microbial_genus = {
+        "dataset": ['microbial_genus_sig_highab'],
+        'saved_models_path': [AD_microbial_genus3],
+        'load_best': [True,],
+        'nb_MC_samples': [10**4],
+        'epsilon': [1e-8, 1e-6],
+        'verbose': [True],
+        'seed': [seed],
+}
+param_list_AD_microbial_genus += get_parameter_array(
+        param_dict=param_dict_AD_microbial_genus)
+
+
+
+
+

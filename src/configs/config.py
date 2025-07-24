3d46f78d21e645b50236f58f8b12aa85d9444586
Florian Ofenheimer-Krach <florian.krach@me.com>
2025-07-24 15:10:11 +0200

    small fix

--- src/configs/config.py
+++ src/configs/config.py
@@ -37,7 +37,14 @@ saved_models_path = '{}saved_models/'.format(data_path)
 abx_ts_filename = "ts_vat19_abx_v20240806"
 microbial_ft_filename = "ft_vat19_anomaly_v20240806_entero_family.tsv"
 
-
+hyperparam_default = {
+    'drift': 2., 'volatility': 0.3, 'mean': 4, 'poisson_lambda': 3.,
+    'speed': 2., 'correlation': 0.5, 'nb_paths': 20000, 'nb_steps': 100,
+    'S0': 1, 'maturity': 1., 'dimension': 1,
+    'obs_perc': 0.1,
+    'scheme': 'euler', 'return_vol': False, 'v0': 1, 'hurst':0.75,
+    'FBMmethod':"daviesharte"
+}
 
 if __name__ == '__main__':
     pass

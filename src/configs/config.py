a8bc65c335e068f958e50b94ed734a1a784c283c
Florian Ofenheimer-Krach <florian.krach@me.com>
2025-08-21 14:07:45 +0200

    bug fixes for correct flags deletion/handling

--- src/configs/config.py
+++ src/configs/config.py
@@ -15,7 +15,8 @@ import numpy as np
 import socket
 
 from configs.config_utils import get_parameter_array, makedirs, \
-    SendBotMessage, data_path, training_data_path, original_data_path
+    SendBotMessage, data_path, training_data_path, original_data_path, \
+    del_all_flags
 
 if 'ada-' not in socket.gethostname():
     SERVER = False
--- src/configs/config_utils.py
+++ src/configs/config_utils.py
@@ -56,3 +56,10 @@ def makedirs(dirname):
         os.makedirs(dirname)
 
 
+def del_all_flags(FLAGS):
+    flags_dict = FLAGS._flags()
+    keys_list = [keys for keys in flags_dict]
+    for keys in keys_list:
+        if key not in ['verbosity']:
+            FLAGS.__delattr__(keys)
+
